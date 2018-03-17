b314613df6878d09c6e113a53615c0151c271980
Taylor Large <segral.jr@gmail.com>
2018-03-17 13:08:58 -0400

    Initial commit

---
+++ src/Workshop.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+
+public class Workshop {
+	
+	String message;
+	boolean finished;
+	boolean[][] mirror, allFalse;
+	
+	ArrayList<boolean[][]> history;
+	
+	int rows, columns;
+	
+	int minLifeToLife, maxLifeToLife, minDeadToLife, maxDeadToLife;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public Workshop() {
+		
+	}
+	
+	public void Workshop(int r, int col) {
+		
+	}
+	
+	public void resetMirror() {
+		
+	}
+	
+	public void resetHistory() {
+		
+	}
+	
+	private boolean isAlive(int k, int j) {
+		boolean tf = false;
+		return tf;
+	}
+	
+	private int neighborCount(int k, int j) {
+		int count = 0;
+		return count;
+	}
+	
+	private boolean equals(boolean[][] arrA, boolean[][] arraB) {
+		boolean tf = false;
+		return tf;
+	}
+	
+	private int listPositionOf(boolean[][] target) {
+		int position = 0;
+		return position;
+	}
+	
+	public void nextGeneration() {
+		
+	}
+	
+	
+
+}
