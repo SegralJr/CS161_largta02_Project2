90b755a8b16d9314a4ec310bccaf2b1617ed1510
Taylor Large <segral.jr@gmail.com>
2018-03-17 14:40:15 -0400

    Edits

--- src/Island.java
+++ src/Island.java
@@ -1,12 +1,52 @@
-public class Island {
+import java.awt.event.ActionListener;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.JFrame;
+
+public class Island extends JFrame implements ActionListener {
 	
-	
+	Workshop shop;
+	JButton[][] population;
+	JTextField northFIeld;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
 	
+	public Island(int rows, int columns, Workshop shop) {
+		
+	}
 	
+	private void enableBoard(boolean flag) {
+		
+	}
+	
+	private void resetBoard() {
+		
+	}
+	
+	public void buildWindow() {
+		
+	}
+	
+	private void displayGeneration(boolean[][] mirror) {
+		
+	}
+	
+	public void actionPerformed(ActionEvent event) {
+		
+		
+	}
 
+}
+
+class ButtonListener implements ActionListener {
+	
+	public void actionPerformed(ActionEvent event) {
+		
+	}
+	
 }
--- src/Workshop.java
+++ src/Workshop.java
@@ -1,50 +1,90 @@
 import java.util.ArrayList;
 
+
 public class Workshop {
 	
-	String message;
-	boolean finished;
-	boolean[][] mirror, allFalse;
+	String message; // Output of game outcome
+	boolean finished; // True if game ends
+	boolean[][] mirror; // Represents current generation, true/false for living/dead squares
+	boolean[][] allFalse;
 	
 	ArrayList<boolean[][]> history;
 	
-	int rows, columns;
+	int rows, columns; // Grid dimensions
 	
-	int minLifeToLife, maxLifeToLife, minDeadToLife, maxDeadToLife;
+	int minLifeToLife, maxLifeToLife, minDeadToLife, maxDeadToLife; // Life paramaters
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
 	
-	public Workshop() {
-		
-	}
-	
-	public void Workshop(int r, int col) {
-		
+	public Workshop(int r, int col) {
+		rows = r;
+		columns = col;
 	}
 	
 	public void resetMirror() {
-		
+		boolean[][] mirror = new boolean[rows][columns];
 	}
 	
 	public void resetHistory() {
-		
+		history.clear();
 	}
 	
 	private boolean isAlive(int k, int j) {
 		boolean tf = false;
+		if (mirror[k][j] == true) {
+			tf = true;
+		}
 		return tf;
 	}
 	
 	private int neighborCount(int k, int j) {
 		int count = 0;
+		
+		if(isAlive(k-1,j-1) == true) {
+			count++;
+		}
+		if(isAlive(k-1, j) == true) {
+			count++;
+		}
+		if(isAlive(k-1, j+1) == true) {
+			count++;
+		}
+		if(isAlive(k, j-1) == true) {
+			count++;
+		}
+		if(isAlive(k, j+1) == true) {
+			count++;
+		}
+		if(isAlive(k+1, j-1) == true) {
+			count++;
+		}
+		if(isAlive(k+1, j) == true) {
+			count++;
+		}
+		if(isAlive(k+1, j+1) == true) {
+			count++;
+		}
+		
 		return count;
 	}
 	
-	private boolean equals(boolean[][] arrA, boolean[][] arraB) {
+	private boolean equals(boolean[][] arrA, boolean[][] arrB) {
 		boolean tf = false;
+		
+		for(int k = 0; k <= arrA.length || k <= arrB.length; k++) {
+			for(int j = 0; j <= arrA.length || j <= arrB.length; j++) {
+				if(arrA[k][j] == arrB[k][j]) {
+					tf = true;
+					if(tf == false) { // Basically a break statement, if one false statement then whole search ends and returns false
+						k = arrA.length + arrB.length;
+					}
+				}
+			}
+		}
+		
 		return tf;
 	}
 	
