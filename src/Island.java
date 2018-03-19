1319b8a80216b37c0f3f7610360462b6ce5060c3
Taylor Large <segral.jr@gmail.com>
2018-03-19 13:44:28 -0400

    Comments and small changes

--- src/Island.java
+++ src/Island.java
@@ -16,15 +16,33 @@ public class Island extends JFrame implements ActionListener {
 		
 	}
 	
-	public Island(int rows, int columns, Workshop shop) {
+	public Island(int rows, int columns, Workshop shop) { // Constructor
+		// Creates title
 		super(">>>>> LIFE GAME BOARD <<<<<");
+		// Initializes shop
 		shop = new Workshop(rows,columns);
-		JButton[][] population = new JButton[rows][columns];
+		
+		// Sets frame parameters
+		setSize(800,450);
+		setLocation(25, 15);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		
+		// INstantiates population to dimensions rows and columns
+		JButton[][] population = new JButton[rows][columns]; 
+		
+		// Iterates over population and instantiates each population entry
 		for(int k = 0; k <= population.length; k++) {
 			for(int j = 0; j < population.length; j++) {
 				population[k][j].setText(String.valueOf(k) + ", " + String.valueOf(j));
+				
+				// Background is set to light gray for each button
+				population[k][j].setBackground(Color.LIGHT_GRAY); 
 			}
 		}
+		
+		// This registered to the array entries
+		
 	}
 	
 	private void enableBoard(boolean flag) {
